b56c255425416a8c42e3288383d9431ef354e8fd
Kethavath Pavan Chandra <63702643+pavankethavath@users.noreply.github.com>
2022-07-12 20:05:58 +0530

    Add files via upload

---
+++ Stock Market Analysis using python.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+pip install yfinance
+
+
+# In[4]:
+
+
+import pandas as pd
+import yfinance as yf
+import datetime
+from datetime import date, timedelta
+today = date.today()
+
+d1 = today.strftime("%Y-%m-%d")
+end_date = d1
+d2 = date.today() - timedelta(days=360)
+d2 = d2.strftime("%Y-%m-%d")
+start_date = d2
+
+data = yf.download('AAPL', 
+                      start=start_date, 
+                      end=end_date, 
+                      progress=False)
+print(data.head())
+
+
+# In[5]:
+
+
+data["Date"] = data.index
+data = data[["Date", "Open", "High", 
+             "Low", "Close", "Adj Close", "Volume"]]
+data.reset_index(drop=True, inplace=True)
+print(data.head())
+
+
+# In[ ]:
+
+
+
+
